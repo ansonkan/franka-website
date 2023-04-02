8302ddd4d763b75a0e9d09e8033e059a0aae73da
Anson Kan <ansonkan@tutanota.com>
2023-04-02 12:01:57 +0200

    revert transpilePackages for three

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ['three'],
+  /**
+   * Note: this seems to be breaking the app but r3f doc is still mentioning this
+   * https://docs.pmnd.rs/react-three-fiber/getting-started/installation#next.js
+   * and `next-transpile-modules` has already be deprecated and replaced by `transpilePackages`
+   *
+   * However, the official next + r3f starter (https://github.com/pmndrs/react-three-next) doesn't have that setup
+   */
+  // transpilePackages: ['three'],
 }
 
-module.exports = nextConfig
+module.exports = withBundleAnalyzer(nextConfig)
