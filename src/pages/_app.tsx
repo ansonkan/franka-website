3d87cea535bb534112274750d717f330c02acbcb
Anson Kan <ansonkan@tutanota.com>
2023-05-04 16:16:10 +0800

    drafted home page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,14 @@ import 'the-new-css-reset/css/reset.css'
 import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
 import Lenis from '@studio-freight/lenis'
+import Link from 'next/link'
+import { RealViewport } from '@/components/real-viewport'
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
+import dynamic from 'next/dynamic'
 import { gsap } from 'gsap'
 import { raf } from '@studio-freight/tempus'
 import { robotoFlex } from '@/fonts'
+import { useDebug } from '@studio-freight/hamo'
 import { useEffect } from 'react'
 import { useFrame } from '@studio-freight/hamo'
 import { useStore } from '@/lib/use-store'
@@ -22,7 +26,14 @@ if (typeof window !== 'undefined') {
   }, 0)
 }
 
+const Stats = dynamic(
+  () => import('@/components/stats').then(({ Stats }) => Stats),
+  { ssr: false }
+)
+
 export default function App({ Component, pageProps }: AppProps) {
+  const debug = useDebug()
+
   const [lenis, setLenis] = useStore((state) => [state.lenis, state.setLenis])
 
   useEffect(() => {
@@ -44,8 +55,25 @@ export default function App({ Component, pageProps }: AppProps) {
   })
 
   return (
-    <div className={robotoFlex.className}>
-      <Component {...pageProps} />
-    </div>
+    <>
+      <div className={robotoFlex.className}>
+        {debug && <Stats />}
+
+        <nav className="header">
+          <h1 className="name">Franka Robin Zweydinger</h1>
+
+          <div className="links">
+            <Link href="/portfolio">Portfolio</Link>
+            <Link href="/advertising">Advertising</Link>
+            <Link href="/contact">Contact</Link>
+            <Link href="/social-media">Social Media</Link>
+          </div>
+        </nav>
+
+        <Component {...pageProps} />
+      </div>
+
+      <RealViewport />
+    </>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,10 @@
 import { GetStaticProps, NextPage } from 'next'
-import { useEffect, useMemo, useRef } from 'react'
+import { useEffect, useRef } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
-import cn from 'clsx'
 import { gql } from '@/lib/contentful-gql'
 import gsap from 'gsap'
-import { mapRange } from '@/lib/maths'
 import s from './index.module.scss'
 import { useRect } from '@studio-freight/hamo'
 import { useScroll } from '@/lib/use-scroll'
@@ -30,59 +28,152 @@ interface IndexProps {
   projects: Project[]
 }
 
+const SCROLL_DISTANCE_FACTOR = 5
+const PREVIEW_SIDE_IMAGES_POSITIONS = [
+  ['translate(-140%, -10%)', 'translate(25%, -60%)'],
+  ['translate(-150%, 20%)', 'translate(35%, -120%)'],
+  ['translate(-130%, -120%)', 'translate(40%, 20%)'],
+  ['translate(-120%, 30%)', 'translate(60%, -60%)'],
+]
+
 const Index: NextPage<IndexProps> = ({ projects }) => {
+  const lenis = useStore(({ lenis }) => lenis)
+
   const [sampleRectRef, sampleRect] = useRect()
   const scrollDivRef = useRef<HTMLDivElement>(null)
-  const linksRef = useRef<HTMLUListElement>(null)
-  const gotosRef = useRef<HTMLUListElement>(null)
+  const linkListRef = useRef<HTMLUListElement>(null)
+  const gotoListRef = useRef<HTMLUListElement>(null)
+
+  const previewImages = useRef<
+    Array<{ group: HTMLDivElement; children: HTMLDivElement[] }>
+  >([])
+
+  const setupScrollTrigger = useRef(false)
+
+  useEffect(() => {
+    if (setupScrollTrigger.current) return
+
+    document
+      .querySelectorAll<HTMLDivElement>(`.${s.previewGroup}`)
+      .forEach((group, i) => {
+        const children: HTMLDivElement[] = []
+        group
+          .querySelectorAll<HTMLDivElement>(`.${s.previewImage}`)
+          .forEach((node) => children.push(node))
+
+        previewImages.current[i] = { group, children }
+      })
+
+    setupScrollTrigger.current = true
+  }, [])
 
   useScroll(({ scroll }) => {
     if (
       !scrollDivRef.current ||
       !sampleRect ||
-      !gotosRef.current ||
-      !linksRef.current
+      !gotoListRef.current ||
+      !linkListRef.current
     )
       return
 
-    scrollDivRef.current.style.setProperty(
-      'height',
-      `${window.innerHeight + sampleRect.height * (projects.length - 1) * 2}px`
-    )
+    gsap.set(scrollDivRef.current, {
+      height:
+        window.innerHeight +
+        sampleRect.height * (projects.length - 1) * SCROLL_DISTANCE_FACTOR,
+    })
 
-    gsap.to([linksRef.current, gotosRef.current], {
-      y: -scroll / 2,
+    gsap.to([linkListRef.current, gotoListRef.current], {
+      y: -scroll / SCROLL_DISTANCE_FACTOR,
       duration: 0,
       ease: 'none',
     })
+
+    gsap.to(`.${s.previewsContainer}`, {
+      y:
+        (-scroll / SCROLL_DISTANCE_FACTOR) *
+        (window.innerHeight / sampleRect.height),
+    })
   })
+
+  console.log('projects: ', projects)
 
   return (
     <main>
       <Head>
-        <title>Franka</title>
-        <meta name="description" content="Franka" />
+        <title>Franka Robin Zweydinger</title>
+        <meta name="description" content="Franka Robin Zweydinger" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <div ref={scrollDivRef} />
 
-      <div className={s.base}>
-        <ul className={s.list} ref={gotosRef}>
+      <div className={s.previewsContainer}>
+        {projects.map(
+          (
+            { sys: { id }, thumbnail, photosCollection: { items }, title },
+            i
+          ) => (
+            <div key={id + i} className={s.previewGroup}>
+              {[thumbnail, ...items]
+                .slice(0, 3)
+                .map(({ url, width, height }, j) => {
+                  const isThumbnail = j === 0
+
+                  return (
+                    <div
+                      key={id + url + j}
+                      className={s.previewImage}
+                      style={{
+                        aspectRatio: width / height,
+                        width: isThumbnail ? '60vw' : '30vw',
+                        height: `calc(${
+                          isThumbnail ? 60 : 30
+                        } * var(--vh, 1vh))`,
+                        transform:
+                          j === 0
+                            ? undefined
+                            : PREVIEW_SIDE_IMAGES_POSITIONS[
+                                (i + 1) % PREVIEW_SIDE_IMAGES_POSITIONS.length
+                              ][j % 2],
+                      }}
+                    >
+                      <Image src={url} fill sizes="40vw" alt={title} />
+                    </div>
+                  )
+                })}
+            </div>
+          )
+        )}
+      </div>
+
+      <div className={s.menuContainer}>
+        <ul className={s.list} ref={gotoListRef}>
           {projects.map(({ sys: { id }, title }, i) => (
-            <li key={id + i} className={s.goto} onClick={() => console.log(id)}>
+            <li
+              key={id + i}
+              className={s.goto}
+              onClick={() => {
+                lenis?.scrollTo(
+                  sampleRect.height * i * SCROLL_DISTANCE_FACTOR,
+                  {
+                    // Note: without this, `scrollTo` seems to be never ending and keeps triggering `scroll` event
+                    duration: 1,
+                  }
+                )
+              }}
+            >
               {title}
             </li>
           ))}
         </ul>
 
-        <div className={s.target}>
+        <div className={s.menuLinkArea}>
           <div className={s.sample} ref={sampleRectRef}>
             BASE
           </div>
 
-          <ul className={s.list} ref={linksRef}>
+          <ul className={s.list} ref={linkListRef}>
             {projects.map(({ sys: { id }, title }, i) => (
               <li key={id + i}>
                 <Link className={s.link} href={`/projects/${id}`}>
