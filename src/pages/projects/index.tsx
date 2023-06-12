bb88039e4a2d752c930c25c3fa8a4ed2e5cc2e94
Anson Kan <ansonkan@tutanota.com>
2023-06-12 20:51:08 +0800

    a few small touch ups

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -7,8 +7,6 @@ import { getImgColor } from '@/lib/get-img-color'
 import { m } from 'framer-motion'
 import { mBlurProps } from '@/constants'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-import { useGsap } from '@/lib/use-gsap'
-import { useTranslation } from 'next-i18next'
 
 interface ProjectsPageProps {
   projects: NonNullable<ProjectsQuery>
@@ -16,87 +14,40 @@ interface ProjectsPageProps {
 }
 
 const ProjectsPage: NextPage<ProjectsPageProps> = ({ projects, colorMap }) => {
-  const { t } = useTranslation('common')
-  const gsap = useGsap()
-
-  const onMouseEnter = (id: string) => {
-    gsap?.to(`.project-row:not([data-project-id="${id}"])`, { opacity: 0.4 })
-  }
-
-  const onMouseLeave = () => {
-    gsap?.to('.project-row', { opacity: 1 })
-  }
-
   return (
-    <main className="projects-page normalPageRoot">
+    <main className="normalPageRoot projects-page">
       <m.div {...mBlurProps}>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>{t('plain-text.projects.table.header.title')}</th>
-              <th>{t('plain-text.projects.table.header.previews')}</th>
-            </tr>
-          </thead>
+        <ol className="project-list">
+          {projects.projectsCollection?.items.map((project) => {
+            if (!project) return
 
-          <tbody>
-            {projects.projectsCollection?.items.map((project) => {
-              if (!project) return
+            const { sys, title, previewsCollection } = project
 
-              const { sys, title, previewsCollection } = project
+            const href = `/projects/${sys.id}`
 
-              const href = `/projects/${sys.id}`
+            return (
+              <li key={sys.id} className="project-list-item">
+                <Link href={href} className="previewsCol" scroll={false}>
+                  {previewsCollection?.items.map((photo, i) => {
+                    if (!photo || !photo.url) return
 
-              return (
-                <tr
-                  key={sys.id}
-                  data-project-id={sys.id}
-                  className="project-row"
-                >
-                  <td>
-                    <Link
-                      href={href}
-                      scroll={false}
-                      onMouseEnter={() => onMouseEnter(sys.id)}
-                      onMouseLeave={onMouseLeave}
-                    >
-                      <h2 className="title">{title}</h2>
-                    </Link>
-                  </td>
-
-                  <td>
-                    <Link
-                      href={href}
-                      className="previewsCol"
-                      scroll={false}
-                      onMouseEnter={() => onMouseEnter(sys.id)}
-                      onMouseLeave={onMouseLeave}
-                    >
-                      {previewsCollection?.items.map((photo, i) => {
-                        if (!photo || !photo.url) return
-
-                        return (
-                          <figure
-                            key={photo.sys.id}
-                            className="previewsImgWrapper"
-                          >
-                            <FillImage
-                              src={photo.url}
-                              alt={`${title || ''} ${i}`}
-                              sizes="(min-width: 800px) 10vh, 50px"
-                              color={colorMap[photo.url]}
-                              quality={30}
-                              isSquare
-                            />
-                          </figure>
-                        )
-                      })}
-                    </Link>
-                  </td>
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
+                    return (
+                      <figure key={photo.sys.id} className="previewsImgWrapper">
+                        <FillImage
+                          src={photo.url}
+                          alt={`${title || ''} ${i}`}
+                          sizes="20vw"
+                          color={colorMap[photo.url]}
+                          isSquare
+                        />
+                      </figure>
+                    )
+                  })}
+                </Link>
+              </li>
+            )
+          })}
+        </ol>
       </m.div>
     </main>
   )
